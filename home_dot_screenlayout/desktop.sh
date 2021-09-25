96006cb1ab9434e7a476479a5d37196efe1372ef
dmolin <davide.molin@gmail.com>
2021-09-25 14:10:33 +0100

    add screenlayout config file

---
+++ home_dot_screenlayout/desktop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# configuration saved by arandr
+# This file is referenced in /etc/lightdm/lightdm.conf with the following lines
+#
+# [Seat:*]
+# display-setup-script=/home/user/.screenlayout/desktop.sh
+#
+xrandr --output DP-0 --mode 2560x1440 --pos 0x0 --rotate right --output DP-1 --off --output HDMI-0 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 3840x2160 --pos 1440x544 --rotate normal --output DP-5 --off
