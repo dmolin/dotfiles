b9b2e545ef34d4e221f23003f832418b4db3c6db
dmolin <davide.molin@gmail.com>
2021-12-17 22:23:01 +0000

    update picom

--- .config/picom/start_picom.sh
+++ .config/picom/start_picom.sh
@@ -4,5 +4,5 @@ PICOM=$(pidof picom)
 if [ $PICOM > 0 ]; then exit; fi
 sleep 2
 #picom --experimental-backends -b --vsync --log-file /tmp/picom.log &
-picom -b --vsync --log-file /tmp/picom.log &
+picom --experimental-backends -b --vsync --log-file /tmp/picom.log &
 
