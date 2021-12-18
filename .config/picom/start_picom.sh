5f6368db108908adc6cc56dec2a306ffc28c930c
dmolin <davide.molin@gmail.com>
2021-12-18 13:57:07 +0000

    fix issue

--- .config/picom/start_picom.sh
+++ .config/picom/start_picom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 :
 PICOM=$(pidof picom)
-if [ $PICOM > 0 ]; then exit; fi
+if [ $PICOM -gt 0 ]; then exit; fi
 sleep 2
 #picom --experimental-backends -b --vsync --log-file /tmp/picom.log &
 picom --experimental-backends -b --vsync --log-file /tmp/picom.log &
