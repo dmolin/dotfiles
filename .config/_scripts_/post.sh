1565ec6cdad389115035abd2511a02c5f8d48b63
dmolin <davide.molin@gmail.com>
2022-02-17 16:53:31 +0000

    Adding script for Wacom

--- .config/_scripts_/post.sh
+++ .config/_scripts_/post.sh
@@ -3,6 +3,10 @@ if [ "$outcome" != "0" ]; then
   exit 1
 fi
 
-echo "Mapping tablet pen to screen"
-id=`xinput | grep "stylus" | awk '{ print $6 }' | awk 'BEGIN {FS="="} { print $2 }'`
+echo "Mapping HUION tablet pen to screen"
+id=`xinput | grep "HID 256c:006d stylus" | awk '{ print $6 }' | awk 'BEGIN {FS="="} { print $2 }'`
 xinput map-to-output ${id} HDMI-1
+
+echo "Mapping WACOM Intuous (if available) to main screen"
+id=`xinput | grep "Intuos Pro M Pen stylus" | awk '{ print $9 }' | awk 'BEGIN {FS="="} { print $2 }'`
+xinput map-to-output ${id} DP-4 
