e0b3af8ac001dc3c673a52deee90654264d41c86
dmolin <davide.molin@gmail.com>
2022-03-08 16:36:29 +0000

    disabling wacom checks

--- .config/_scripts_/post.sh
+++ .config/_scripts_/post.sh
@@ -7,6 +7,6 @@ echo "Mapping HUION tablet pen to screen"
 id=`xinput | grep "HID 256c:006d stylus" --max-count=1 | awk '{ print $6 }' | awk 'BEGIN {FS="="} { print $2 }'`
 xinput map-to-output ${id} HDMI-1
 
-echo "Mapping WACOM Intuous (if available) to main screen"
-id=`xinput | grep "Intuos Pro M Pen stylus" | awk '{ print $9 }' | awk 'BEGIN {FS="="} { print $2 }'`
-xinput map-to-output ${id} DP-4 
+#echo "Mapping WACOM Intuous (if available) to main screen"
+#id=`xinput | grep "Intuos Pro M Pen stylus" | awk '{ print $9 }' | awk 'BEGIN {FS="="} { print $2 }'`
+#xinput map-to-output ${id} DP-4 
