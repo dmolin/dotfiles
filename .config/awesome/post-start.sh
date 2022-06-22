5d69eebb087e2715e5d457c1ddf975a743b0ad4f
dmolin <davide.molin@gmail.com>
2022-06-22 09:05:25 +0100

    latest tweaks

--- .config/awesome/post-start.sh
+++ .config/awesome/post-start.sh
@@ -2,5 +2,9 @@
 :
 ~/.config/awesome/scripts/killAndWait.sh pcloud
 ~/Applications/pcloud &>/dev/null &
+
+#~/.config/awesome/scripts/killAndWait.sh filen
+#~/Applications/filen-setup.AppImage &>/dev/null &
+
 ~/.config/awesome/scripts/killAndWait.sh redshift
 redshift -P &>/dev/null &
