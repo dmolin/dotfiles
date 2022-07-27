3d180e400e270bd892d544296bf97fa154df9e48
dmolin <davide.molin@gmail.com>
2022-07-27 21:47:29 +0100

    improve config

---
+++ .config/i3/post.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+:
+~/.config/i3/killAndWait.sh pcloud
+~/Applications/pcloud &>/dev/null &
+
+#~/.config/awesome/scripts/killAndWait.sh filen
+#~/Applications/filen-setup.AppImage &>/dev/null &
+
+~/.config/i3/killAndWait.sh redshift
+redshift -P &>/dev/null &
