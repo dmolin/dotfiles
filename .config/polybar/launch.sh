c5224a66a14c76c83dfd6cd3deb088242354f983
dmolin <davide.molin@gmail.com>
2022-07-31 10:07:47 +0100

    more tweaking; using picom with rounded corners; improving bar; adding grub customization

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -15,7 +15,7 @@ killall -q polybar
 
 DISPLAYS=`~/.config/_scripts_/set_displays.sh`
 MAIN_DISPLAY=`echo $DISPLAYS | awk '{print $1}'`
-TOP_DISPLAY=`echo $DISPLAYS | awk '{print $2}'`
+#TOP_DISPLAY=`echo $DISPLAYS | awk '{print $2}'`
 #THIRD_DISPLAY=`echo $DISPLAYS | awk '{print $3}'`
 
 export MAIN_DISPLAY
@@ -25,14 +25,11 @@ export TOP_DISPLAY
 echo "  -> waiting a bit..."
 sleep 2
 
-# Wait until the process has been properly shut down
-#while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
-
 # Launch bar1
 echo "  -> starting new process..." | tee -a /tmp/polybar1.log
 MAIN_DISPLAY=$MAIN_DISPLAY polybar -c ~/.config/polybar/config.ini.$VARIANT main >> /tmp/polybar1.log 2>&1 & disown
 
-if [ "$MAIN_DISPLAY" != "$TOP_DISPLAY" ]; then
-  echo " -> starting bar on secondary display..." | tee -a /tmp/polybar1.log
-  TOP_DISPLAY=$TOP_DISPLAY polybar -c ~/.config/polybar/config.ini.$VARIANT left >> /tmp/polybar1.log 2>&1 & disown
-fi
+#if [ "$MAIN_DISPLAY" != "$TOP_DISPLAY" ]; then
+  #echo " -> starting bar on secondary display..." | tee -a /tmp/polybar1.log
+  #TOP_DISPLAY=$TOP_DISPLAY polybar -c ~/.config/polybar/config.ini.$VARIANT left >> /tmp/polybar1.log 2>&1 & disown
+#fi
