b0c8ec5d073e61bee98e1921d11c76213d0dcda5
dmolin <davide.molin@gmail.com>
2022-08-22 14:38:25 +0100

    improve picom launcher

--- .config/picom/start_picom.sh
+++ .config/picom/start_picom.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 :
 PICOM=$(pidof picom)
-if [ $PICOM -gt 0 ]; then exit; fi
+if [ ! -z "$PICOM" ]; then exit; fi
 sleep 2
 picom --experimental-backends -b --vsync --log-file /tmp/picom.log &
 
