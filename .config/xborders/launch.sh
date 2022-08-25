fb4163fe8e57e7f006276462d71092e457b8f006
dmolin <davide.molin@gmail.com>
2022-08-25 21:03:34 +0100

    fix startup scripts

--- .config/xborders/launch.sh
+++ .config/xborders/launch.sh
@@ -2,6 +2,6 @@
 :
 pkill xborders
 cd ~/.config/xborders/xborders
-git pull
+git submodule update --init
 ~/.config/xborders/xborders/xborders --border-radius 8 --border-width 3 --border-red 250 --border-green 131 --border-blue 14 1>&2 2>/dev/null &
 
